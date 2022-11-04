32727e503fa2653ba1d9dea0a1da6f8f01852057
manuelmenama <manuel.mena090589@gmail.com>
2022-11-04 12:45:05 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log('Ciao vuejs!');
+
+
+const {createApp} = Vue;
+
+createApp({
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    console.log("Mounted 'vue-carousel'");
+  }
+}).mount("#app");
+
