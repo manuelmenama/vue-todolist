0290c99a9cebebceb6890e5b68db04b7f212ad58
manuelmenama <manuel.mena090589@gmail.com>
2022-11-04 17:56:15 +0100

    almost ended

--- js/script.js
+++ js/script.js
@@ -7,24 +7,46 @@ createApp({
   data() {
     return {
 
+      indexCounter: 0,
+      newTaskString: "",
+
       taskThings: [
         {
           name: "Portare fuori il cane",
-          taskDone: false
+          taskDone: false,
+          errorMsg: ''
         },
         {
           name: "Portare fuori il gatto",
-          taskDone: true
+          taskDone: true,
+          errorMsg: ''
+
         },
         {
           name: "Portare fuori il nonno",
-          taskDone: false
+          taskDone: false,
+          errorMsg: ''
         },
       ]
     }
   },
   methods: {
-
+    deleteTask(index) {
+      this.taskThings[index].errorMsg = '';
+      if(!this.taskThings[index].taskDone){
+        this.taskThings[index].errorMsg = "Devi prima completare il task!"
+      }else{
+        this.taskThings.splice(index, 1)
+      }
+    },
+    addTask() {
+      const newTask = {
+        name: this.newTaskString,
+        taskDone: false
+      }
+      this.taskThings.unshift(newTask);
+      this.newTaskString = '';
+    }
   },
   mounted() {
     console.log("Mounted 'vue-carousel'");
